a4f44c30faf73a356c5c5614d717a9bf15c221f4
NSkyeKirsch <73858119+NSkyeKirsch@users.noreply.github.com>
2022-03-29 22:13:32 -0400

    finished q1

---
+++ hw6_q1.py
@@ -0,0 +1,60 @@
+"""
+Author: Noa Kirschbaum
+Assignment / Part: HW6 - Q1
+Date due: 2022-03-31
+I pledge that I have completed this assignment without
+collaborating with anyone else, in conformance with the
+NYU School of Engineering Policies and Procedures on
+Academic Misconduct.
+"""
+
+def print_shifted_triangle(height, margin, symbol):
+    """
+    :param height: number of lines triangle will have
+    :param margin: margin in front of triangle
+    :param symbol: the chosen character
+    :return: print triangle
+    """
+    orig_margin = margin
+    final_str = ""
+    final_margin = ""
+    rows = 1
+    while rows <= height*2:
+        for m in range(margin + height - 1):
+            final_margin += "_"
+        for i in range(1,rows+1):
+            final_str += symbol
+        if(len(final_str)%2 != 0):
+            print(final_margin + final_str)
+            if margin + height > orig_margin:
+                margin -= 1
+        rows += 1
+        final_str = ""
+        final_margin = ""
+
+
+
+
+def print_pine_tree(levels, symbol):
+    """
+    :param levels: number of triangles of increasing sizes
+    :param symbol: the character
+    :return: big pine tree using print_shifted_triangle
+    """
+    x = levels - 1
+    for i in range(2,levels+2):
+        print_shifted_triangle(i,x,symbol)
+        x -= 1
+
+
+def main():
+    """""
+    test code here
+    """""
+
+    input_num_trees = int(input("Enter a positive, non-zero, integer: "))
+    input_character = input("Enter a non-whitespace, non-alphanumeric character: ")
+
+    print_pine_tree(input_num_trees,input_character)
+
+main()
